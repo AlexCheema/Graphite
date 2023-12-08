c1951bd19b097ab3435d5e9a5f7fb9b6270ff021
Miyamura80 <eitomiyamura@gmail.com>
2023-12-08 17:57:26 +0000

    initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygraphite',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your package dependencies here
+    ],
+    # additional metadata
+)
