172a9a0f05af161b8c18d640cb7b1661caf95a45
Miyamura80 <eitomiyamura@gmail.com>
2023-12-10 01:48:00 +0000

    demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
+from graphite.setup_custom import bfs
 import networkx
+import pickle
 
 # Construct a graph G
 G = networkx.DiGraph()
-edges_numerical = [(0, 1), (0, 2), (1, 3), (1, 4), (2, 5), (4, 6), (5, 7)]
-G.add_edges_from(edges_numerical)
-u_index = 0
 
+with open("graph.gpickle", "rb") as f:
+        G = pickle.load(f)
 
-
+bfs(G, 0)
